4b2d6e56d67c7105da49e1047327fc9258b5d1d6
Caroline Desprat <cdesprat@icij.org>
2022-01-10 10:59:54 +0000

    test: add async test server class
    Co-authored-by: Bruno Thomas <bamthomas@users.noreply.github.com>

---
+++ test/server.py
@@ -0,0 +1,44 @@
+import asyncio
+from typing import Optional, List
+
+import uvicorn
+
+
+class UvicornTestServer(uvicorn.Server):
+    """Uvicorn test server
+
+    Usage:
+        @pytest.fixture
+        server = UvicornTestServer()
+        await server.up()
+        yield
+        await server.down()
+    """
+
+    def __init__(self, app, host='127.0.0.1', port=12345):
+        """Create a Uvicorn test server
+
+        Args:
+            app (FastAPI, optional): the FastAPI app. Defaults to main.app.
+            host (str, optional): the host ip. Defaults to '127.0.0.1'.
+            port (int, optional): the port. Defaults to PORT.
+        """
+        self._serve_task = None
+        self._startup_done = asyncio.Event()
+        super().__init__(config=uvicorn.Config(app, host=host, port=port))
+
+    async def startup(self, sockets: Optional[List] = None) -> None:
+        """Override uvicorn startup"""
+        await super().startup(sockets=sockets)
+        self.config.setup_event_loop()
+        self._startup_done.set()
+
+    async def up(self) -> None:
+        """Start up server asynchronously"""
+        self._serve_task = asyncio.create_task(self.serve())
+        await self._startup_done.wait()
+
+    async def down(self) -> None:
+        """Shut down server asynchronously"""
+        self.should_exit = True
+        await self._serve_task
--- test/test_api.py
+++ test/test_api.py
@@ -1,35 +1,24 @@
-from multiprocessing import Process
-from time import sleep
-
 import pytest
-import uvicorn
 from yarl import URL
 
 from dsnetclient.api import DsnetApi
+from test.server import UvicornTestServer
+
 server = None
 
 
-def setup_module(_):
+@pytest.fixture
+async def startup_and_shutdown_server():
     global server
 
-    server = Process(target=uvicorn.run,
-                     args=('dsnetserver.main:app',),
-                     kwargs={
-                         "host": "127.0.0.1",
-                         "port": 12345,
-                         "log_level": "info"},
-                     daemon=True)
-    server.start()
-    sleep(1)
-
-
-def teardown_module(_):
-    global server
-    server.terminate()
+    """Start server as test fixture and tear down after test"""
+    server = UvicornTestServer('dsnetserver.main:app')
+    await server.up()
+    yield
+    await server.down()
 
 
 @pytest.mark.asyncio
-async def test_root():
-    assert await DsnetApi(URL('http://localhost:12345')).get_server_version() == {'message': 'Datashare Network Server version 0.1.0'}
-
-
+async def test_root(startup_and_shutdown_server):
+    assert (await DsnetApi(URL('http://localhost:12345')).get_server_version()) == {
+        'message': 'Datashare Network Server version 0.1.0'}
