0e51d8b4aec27fd1459fe7587402a72cc32351e0
Bruno Thomas <bthomas@icij.org>
2022-02-28 13:20:58 +0000

    feat: add a small ES index ICIJ/datashare-network-core#6

--- dsnetclient/index.py
+++ dsnetclient/index.py
@@ -1,6 +1,8 @@
 import abc
 from typing import Set, List
 
+from elasticsearch import AsyncElasticsearch
+
 
 class Index(metaclass=abc.ABCMeta):
     """
@@ -14,10 +16,46 @@ class Index(metaclass=abc.ABCMeta):
         :return:
         """
 
+    """
+    close index connection
+    """
+    @abc.abstractmethod
+    async def close(self) -> None:
+        """
+        closes the connection(s) to the index server
+        """
+
 
 class ElasticsearchIndex(Index):
+    def __init__(self, aes: AsyncElasticsearch):
+        self.aes = aes
+
     async def search(self, query: bytes) -> List[bytes]:
-        pass
+        resp = await self.aes.search(index="local-datashare", body=self.query_body_from_string(query.decode()))
+        return [hit["_source"]["mention"].encode() for hit in resp["hits"]["hits"]]
+
+    def query_body_from_string(self, query: str) -> dict:
+        return {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "match": {
+                                "type": "NamedEntity"
+                            }
+                        },
+                        {
+                            "query_string": {
+                                "query": query
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+
+    async def close(self):
+        await self.aes.close()
 
 
 class MemoryIndex(Index):
@@ -27,3 +65,7 @@ class MemoryIndex(Index):
     async def search(self, query: bytes) -> List[bytes]:
         term_list = set(query.decode().split())
         return list(map(lambda s: s.encode(), term_list.intersection(self.entities)))
+
+    async def close(self) -> None:
+        pass
+
