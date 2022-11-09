2abdea6174b78de1035daebb30a4317dcf6626c2
Bruno Thomas <bthomas@icij.org>
2022-11-09 15:48:35 +0000

    feat: add decode for QueryEncoder ICIJ/datahare-network-core#14
    Co-authored-by: Laurent Girod <laurent-girod@users.noreply.github.com>

--- dsnetclient/query_encoder.py
+++ dsnetclient/query_encoder.py
@@ -3,25 +3,39 @@ from typing import Tuple, List
 
 from dsnet.mspsi import MSPSIQuerier
 from petlib.bn import Bn
+from sscred import packb, unpackb
 
 
 class QueryEncoder(ABC):
     """Query encoder"""
 
     @abstractmethod
-    def encode(self, query: List[str]) -> Tuple[bytes, Bn]:
+    def encode(self, query: List[bytes]) -> Tuple[Bn, bytes]:
         """
-        encode query
+        @param query : list of keywords ascii bytes
         return a tuple payload, secret
+        """
+
+    @abstractmethod
+    def decode(self, secret: Bn, reply: bytes) -> List[bytes]:
+        """
+        decode reply
+        return a list of decoded keywords (or kwds hashes)
         """
 
 
 class MSPSIEncoder(QueryEncoder):
-    def encode(self, kwds: List[str]) -> Tuple[bytes, Bn]:
+    def encode(self, kwds: List[bytes]) -> Tuple[Bn, bytes]:
         secret, kwds_enc = MSPSIQuerier.query(kwds)
-        return b'\n'.join(kwds_enc), secret
+        return secret, packb(kwds_enc)
+
+    def decode(self, secret: Bn, reply: bytes) -> List[bytes]:
+        return MSPSIQuerier.decode_reply(secret, unpackb(reply))
 
 
 class LuceneEncoder(QueryEncoder):
-    def encode(self, query: List[str]) -> Tuple[bytes, Bn]:
-        return '\n'.join(query).encode(), Bn()
+    def encode(self, kwds: List[bytes]) -> Tuple[Bn, bytes]:
+        return Bn(), packb(kwds)
+
+    def decode(self, _secret: Bn, reply: bytes) -> List[bytes]:
+        return unpackb(reply)
